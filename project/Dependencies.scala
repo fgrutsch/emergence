497a0e335266035212e6275f93bf91f6bd967741
Fabian Grutsch <f.grutsch@gmail.com>
2023-01-08 01:40:08 +0000

    Update scalatest to 3.2.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     "io.circe"                        %% "circe-core"                     % circeVersion,
     "io.circe"                        %% "circe-parser"                   % circeVersion,
     "io.circe"                        %% "circe-yaml"                     % "0.14.2",
-    "org.scalatest"                   %% "scalatest"                      % "3.2.14" % Test,
+    "org.scalatest"                   %% "scalatest"                      % "3.2.15" % Test,
     "org.typelevel"                   %% "cats-effect"                    % "3.4.3",
     "org.typelevel"                   %% "log4cats-slf4j"                 % "2.5.0"
   )
