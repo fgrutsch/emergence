c1c5fde5121f105b77a3bd270a0aabc1db408495
Fabian Grutsch <f.grutsch@gmail.com>
2023-02-12 01:41:18 +0000

    Update circe-core, circe-parser to 0.14.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  private val circeVersion      = "0.14.3"
+  private val circeVersion      = "0.14.4"
   private val fs2Version        = "3.4.0"
   private val sttpClientVersion = "3.8.2"
 
